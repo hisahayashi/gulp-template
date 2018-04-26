dce3f98f0d310bced35393477d26c5ca1525c979
Hisayoshi Hayashi <hayashi@hisayoshihayashi.com>
2018-04-26 23:27:30 +0900

    update package

--- build/assets/js/bundle.js
+++ build/assets/js/bundle.js
@@ -1,1 +1,85 @@
-!function(t){var r={};function s(e){if(r[e])return r[e].exports;var n=r[e]={i:e,l:!1,exports:{}};return t[e].call(n.exports,n,n.exports,s),n.l=!0,n.exports}s.m=t,s.c=r,s.d=function(e,n,t){s.o(e,n)||Object.defineProperty(e,n,{configurable:!1,enumerable:!0,get:t})},s.r=function(e){Object.defineProperty(e,"__esModule",{value:!0})},s.n=function(e){var n=e&&e.__esModule?function(){return e.default}:function(){return e};return s.d(n,"a",n),n},s.o=function(e,n){return Object.prototype.hasOwnProperty.call(e,n)},s.p="",s(s.s="./src/assets/ts/entry.ts")}({"./src/assets/ts/entry.ts":function(module,exports){eval("class Main {\n    constructor() {\n        console.log('Hello! Node.js × TypeScript from Class');\n    }\n}\nconst main = new Main();\n\n\n//# sourceURL=webpack:///./src/assets/ts/entry.ts?")}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/assets/ts/entry.ts");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/assets/ts/entry.ts":
+/*!********************************!*\
+  !*** ./src/assets/ts/entry.ts ***!
+  \********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("class Main {\n    constructor() {\n        console.log('Hello! Node.js × TypeScript from Class');\n    }\n}\nconst main = new Main();\n\n\n//# sourceURL=webpack:///./src/assets/ts/entry.ts?");
+
+/***/ })
+
+/******/ });
--- build/assets/js/include.js
+++ build/assets/js/include.js
@@ -1,1 +1,36 @@
-var env={path:{root:"http://example.com/",endpoint:"http://api.example.com/"},meta:{title:"test",description:"",keywords:""},facebook:{appid:"0000000000000000"},ga:{id:"UA-xxx-1"},production:!0};window.fbAsyncInit=function(){FB.init({appId:env.facebook.appid,xfbml:!0,version:"v2.2"})},function(e,t,a){var n,o=e.getElementsByTagName(t)[0];e.getElementById(a)||((n=e.createElement(t)).id=a,n.src="//connect.facebook.net/ja_JP/sdk.js",o.parentNode.insertBefore(n,o))}(document,"script","facebook-jssdk"),function(e,t,a,n,o,c,i){e.GoogleAnalyticsObject=o,e.ga=e.ga||function(){(e.ga.q=e.ga.q||[]).push(arguments)},e.ga.l=1*new Date,c=t.createElement(a),i=t.getElementsByTagName(a)[0],c.async=1,c.src="//www.google-analytics.com/analytics.js",i.parentNode.insertBefore(c,i)}(window,document,"script",0,"ga"),ga("create",env.ga.id,"auto"),ga("send","pageview");
+var env = {"path":{"root":"/","endpoint":"/"},"meta":{"title":"test","description":"","keywords":""},"facebook":{"appid":"0000000000000000"},"ga":{"id":"UA-xxx-1"},"production":false}
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : env.facebook.appid,
+    xfbml      : true,
+    version    : 'v2.2'
+  });
+
+  // FB.Event.subscribe( 'edge.create', function(response) {
+  // });
+
+  // FB.Event.subscribe( 'edge.remove', function(response) {
+  // });
+
+  // FB.Event.subscribe( 'comment.create', function(response) {
+  // });
+
+  // FB.Event.subscribe( 'comment.remove', function(response) {
+  // });
+};
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "//connect.facebook.net/ja_JP/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', env.ga.id, 'auto');
+ga('send', 'pageview');
